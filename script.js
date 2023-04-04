013f9317dfeb18a5e8db64e3c65a0771ff3ac859
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-04-04 17:26:16 +0430

    add some data about peoples doing

--- script.js
+++ script.js
@@ -14,6 +14,50 @@ const inputElevation = document.querySelector('.form__input--elevation');
 //* Project
 /////////////////////////////////////////
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat , lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout{
+    constructor(coords , distance , duration , cadence){
+        super(coords , distance , duration) ;
+        this.cadence = cadence
+        this.calcPace() ;
+    }
+
+    calcPace(){
+        // min / km
+        this.pace = this.duration / this.distance ;
+        return this.pace ; 
+    }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+const run1 = new Running([36.2988797, 50.0148414], 5.2, 24, 178);
+const cycling1 = new Cycling([36.2988797, 50.0148414], 27, 95, 523);
+console.log(run1, cycling1);
+
+
 class App {
   #map;
   #mapEvent;
@@ -84,5 +128,5 @@ class App {
       .openPopup();
   }
 }
- 
+
 const app = new App();
