1efe13edd4fab6f2569665e2a29bac7dbaf3b020
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-04-04 16:33:05 +0430

    add third-library map

--- script.js
+++ script.js
@@ -14,4 +14,27 @@ const inputElevation = document.querySelector('.form__input--elevation');
 //* Project
 /////////////////////////////////////////
 
-console.log();
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    console.log(position);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+  },
+  function () {
+    console.error('Could not access to location');
+  }
+);
